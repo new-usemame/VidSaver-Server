0b718909c3983f915f12a8373cf4928c74289749
anonymous-dev <anonymous@users.noreply.github.com>
2025-12-16 01:33:08 -0500

    Update PID files and refactor server.py for improved readability
    - Updated menubar_app.pid and tray_app.pid to reflect new process IDs.
    - Refactored return statements in server.py for consistent indentation and improved code clarity.
    - Enhanced the launch process for the menubar app and watchdog with clearer structure and logging.

---
+++ .tray_launcher.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "/Users/acoundou/Python Projects/TikTok-Downloader-Server"
+"/Users/acoundou/Python Projects/TikTok-Downloader-Server/venv/bin/python" "/Users/acoundou/Python Projects/TikTok-Downloader-Server/tray_app.py"
